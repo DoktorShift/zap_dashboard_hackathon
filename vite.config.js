a13544accdcee9a363666802c75a175f6af45fe3
pratikpatel <pratikpatelpp802@gmail.com>
2025-07-03 20:22:28 +0530

    Fix content publish paid

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt'],
+      manifest: {
+        name: 'Zap Dashboard',
+        short_name: 'ZapDash',
+        description: 'Lightning-powered dashboard for Nostr and more!',
+        theme_color: '#42b883',
+        icons: [
+          {
+            src: 'dashboard.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'dashboard.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
