968ea604c6f52a648e22318564e3cef9197ba50d
DoktorShift <106493492+DoktorShift@users.noreply.github.com>
2025-09-12 02:34:38 +0200

    Fix duplicate formatNumber function declaration

--- src/components/EngagementMetrics.vue
+++ src/components/EngagementMetrics.vue
@@ -81,16 +81,6 @@ const formatNumber = (num) => {
   }
   return num.toString()
 }
-
-// Format numbers to be user-friendly (1.2k, 1.5M, etc.)
-const formatNumber = (num) => {
-  if (num >= 1000000) {
-    return `${(num / 1000000).toFixed(1)}M`
-  } else if (num >= 1000) {
-    return `${(num / 1000).toFixed(1)}k`
-  }
-  return num.toString()
-}
 </script>
 
 <template>
